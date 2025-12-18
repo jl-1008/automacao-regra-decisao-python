30168f65681082339512573b131fca24df49e894
JorgeLeandro <jorgeleandro1008@gmail.com>
2025-12-18 10:56:43 -0300

    Adiciona validação de tipos e controle de fluxos com loop

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ Este programa recebe o valor e o tipo de uma solicitação
 e define automaticamente seu statuts com base em regras
 de negócio e categorias emergenciais.
 """
+
 # imports 
 # (não são necessários imports adicionais nesse projeto)
 # constantes (politicas de negócio)
 LIMITE_APROVADO = 1000
 VALOR_MAX_EMERGENCIA = 5000
 TIPOS_EMERGENCIA = ["despesa_extra", "acidente", "prejuizo"]
+TIPOS_VALIDOS = ["despesa_extra", "acidente", "prejuizo", "operacional", "planejada"]
+
 
 def definir_status(valor, tipo_solicitacao):
     """
@@ -24,15 +27,35 @@ def definir_status(valor, tipo_solicitacao):
         return "EM ANÁLISE"
     
 def main():
-    # Entrada de dados
-    valor = float(input("Informe o valor da solicitação: "))
-    tipo = input( "Informe o tipo da solicitação"
-                 "(despesa_extra, acidente, prejuizo, operacional, planejada): "
+    while True:
+        try:
+           # Entrada de valor
+           valor = float(input("Informe o valor da solicitação: "))
+
+           if valor <= 0:
+               print("Erro: o valor deve ser maior que zero. ")
+               continue
+           
+           # Entrada do tipo
+           tipo = input( 
+                "Informe o tipo da solicitação"
+                "(despesa_extra, acidente, prejuizo, operacional, planejada): "
             ).lower()
-    # Processamento (regra de negócio)
-    status = definir_status(valor, tipo)
+           
+           if tipo not in TIPOS_VALIDOS:
+              print("Erro: tipo de solicitação inválido.")
+              continue
+           
+           # Processamento (regra de negócio)
+           status = definir_status(valor, tipo)
 
-    #saida 
-    print(f"Status da solicitação: {status}")
+           #saida 
+           print(f"Status da solicitação: {status}")
+           break
+
+        except ValueError:
+            print("Erro: informe um valor numérico válido.")
 
 
+if __name__ == "__main__":
+    main()
